fb2bd0517caf2f58461b7e77d42cd698049570ed
Vitor Paiva Nunes de Paula <brdepaulav@gmail.com>
2025-06-04 16:47:59 -0300

    Quick fixes

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
-import { View, Text, StyleSheet, Dimensions, TextInput } from "react-native";
+import { View, Text, StyleSheet, Dimensions, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform, TouchableWithoutFeedback } from "react-native";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import Background from "../components/Background";
 import LocalButton from "../components/LocalButton";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 const windowWidth = Dimensions.get('window').width;
 
@@ -12,6 +12,9 @@ export default function LoginScreen({ navigation }) {
     const [password, setPassword] = useState(null);
     const [name, setName] = useState(null);
     const [error, showError] = useState(false);
+    const [showPassword, setShowPassoword] = useState(false)
+
+
     useEffect(() => {
         const getLogin = async () => {
             if (JSON.parse(await AsyncStorage.getItem("loginState"))) {
@@ -23,62 +26,71 @@ export default function LoginScreen({ navigation }) {
 
     return (
         <Background>
-            <KeyboardAwareScrollView
-                contentContainerStyle={{ flex: 1 }}
-            >
-                <View style={styles.container}>
-                    <Text style={styles.title}>Login</Text>
-                    <View style={styles.inputContainer}>
-                        <TextInput
-                            style={styles.input}
-                            placeholder="Name"
-                            keyboardType="name-phone-pad"
-                            onChangeText={setName}
-                            placeholderTextColor={'#f0f8ff'}
-                        />
-                    </View>
-                    <View style={styles.inputContainer}>
-                        <TextInput
-                            style={styles.input}
-                            placeholder="Email"
-                            keyboardType="email-address"
-                            onChangeText={setEmail}
-                            placeholderTextColor={'#f0f8ff'}
-                        />
-                    </View>
-                    <View style={styles.inputContainer}>
-                        <TextInput
-                            style={styles.input}
-                            placeholder="Password"
-                            secureTextEntry={true}
-                            onChangeText={setPassword}
-                            placeholderTextColor={'#f0f8ff'}
-                        />
-                    </View>
-                    <View style={styles.buttonContainer}>
-                        <LocalButton onclickFuncion={async () => {
-                            if (name === "adm" && email === "adm@gmail.com" && password === "@adm123") {
-                                await AsyncStorage.setItem("email", email);
-                                if (!parseInt(name) && name) await AsyncStorage.setItem("name", name); else return showError(true);
-                                await AsyncStorage.setItem("password", password);
-                                await AsyncStorage.setItem("loginState", JSON.stringify(true));
-                                navigation.replace('Profile');
-                            } else {
-                                showError(true);
-                            }
-                        }
-                        }
-                        >
-                            <Text style={styles.buttonText}>Enter</Text>
-                        </LocalButton>
-                    </View>
-                    {error && (
-                        <View style={{ margin: 5 }}>
-                            <Text style={{ color: 'red', fontWeight: 'bold' }}>Missing statements or invalid informations</Text>
-                        </View>
-                    )}
+            <View style={styles.container}>
+                <Text style={styles.title}>Login</Text>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Name"
+                        keyboardType="name-phone-pad"
+                        onChangeText={setName}
+                        placeholderTextColor={'#f0f8ff'}
+                    />
                 </View>
-            </KeyboardAwareScrollView>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Email"
+                        keyboardType="email-address"
+                        onChangeText={setEmail}
+                        placeholderTextColor={'#f0f8ff'}
+                    />
+                </View>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Password"
+                        secureTextEntry={!showPassword}
+                        onChangeText={setPassword}
+                        placeholderTextColor={'#f0f8ff'}
+                    />
+                    <TouchableOpacity onPress={() => {
+                        if (showPassword) {
+                            setShowPassoword(false)
+                        } else {
+                            setShowPassoword(true);
+                        }
+                    }}>
+                        <Icon
+                            name={showPassword ? 'eye' : 'eye-off'}
+                            color={'#f0f8ff'}
+                            size={18}
+                        />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <LocalButton onclickFuncion={async () => {
+                        if (name.toLowerCase() === "adm" && email.toLowerCase() === "adm@gmail.com" && password === "@adm123") {
+                            await AsyncStorage.setItem("email", email);
+                            if (!parseInt(name) && name) await AsyncStorage.setItem("name", name); else return showError(true);
+                            await AsyncStorage.setItem("password", password);
+                            await AsyncStorage.setItem("loginState", JSON.stringify(true));
+                            navigation.replace('Profile');
+                        } else {
+                            showError(true);
+                        }
+                    }
+                    }
+                    >
+                        <Text style={styles.buttonText}>Enter</Text>
+                    </LocalButton>
+                </View>
+                {error && (
+                    <View style={{ margin: 5 }}>
+                        <Text style={{ color: 'red', fontWeight: 'bold' }}>Missing statements or invalid informations</Text>
+                    </View>
+                )}
+            </View>
         </Background>
 
     );
@@ -95,7 +107,6 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 24,
         marginBottom: 20,
-        textDecorationLine: 'underline',
         color: '#f0f8ff'
     },
     buttonContainer: {
@@ -109,15 +120,20 @@ const styles = StyleSheet.create({
         textAlign: 'center'
     },
     inputContainer: {
+        marginVertical: 8,
+        flexDirection: 'row',
         backgroundColor: 'transparent',
-    },
-    input: {
-        margin: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
         width: windowWidth * 0.5,
         borderRadius: 5,
         borderColor: '#f0f8ff',
         borderWidth: 2,
-        color: '#f0f8ff'
+        paddingHorizontal: 10
+    },
+    input: {
+        color: '#f0f8ff',
+        flex: 1,
     },
     screen_background: {
         flex: 1,
